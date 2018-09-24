10a54ab5ba05289ea8b6de2b9d84b4b43b0b7335
Simran PC <navigill20@gmail.com>
2018-09-23 22:44:55 -0700

    add styles to sub title

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = (props) => {
   return (
       <div className = "header">
           <h1 className ="header__title">{props.title}</h1>
-          <h2>{props.subTitle}</h2>
+          <h2 className = "header__subtitle">{props.subTitle}</h2>
           </div>
   );
 };
