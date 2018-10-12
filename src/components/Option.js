e74270e847a817d9148ecfef521ec6a92f20ad52
Simranpal Singh <navigill20@gmail.com>
2018-10-12 16:20:42 -0700

    add count to option via props, add css to option text

--- src/components/Option.js
+++ src/components/Option.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Option = (props) =>{
   return (
       <div className = "option">
-        {props.optionText}
+        <p className= "option__text">{props.count}. {props.optionText}</p>
         <button 
           className = "button button--link"
           onClick = {(e)=>{
