4971c143f085af3af95b3d50e8cf4b8902439875
Simranpal Singh <navigill20@gmail.com>
2018-10-13 13:43:08 -0700

    create a route for root and render a component

--- src/app.js
+++ src/app.js
@@ -4,9 +4,15 @@ import {BrowserRouter, Route} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+const ExpenseDashboardPage = () =>(
+    <div>
+        This is from my dashboard component
+    </div>
+);
+
 const routes= (
     <BrowserRouter>
-
+        <Route path="/" component={ExpenseDashboardPage}/>
     </BrowserRouter>
 );
 
