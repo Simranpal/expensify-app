432ed6a5b10fd2b92d7d42783cf823ef970a2684
Simran PC <navigill20@gmail.com>
2018-10-14 22:55:56 -0700

    define components in individual files, import each in app router logic

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,45 +1,12 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
+import AddExpensePage from '../components/AddExpensePage';
+import EditExpensePage from '../components/EditExpensePage';
+import HelpPage from '../components/HelpPage';
+import NotFoundPage from '../components/NotFoundPage';
+import Header from '../components/Header';
 
-const ExpenseDashboardPage = () =>(
-  <div>
-      This is from my dashboard component
-  </div>
-);
-
-const AddExpensePage = () =>(
-  <div>
-      This is from my add expense component
-  </div>
-);
-
-const EditExpensePage = () =>(
-  <div>
-      This is from my EDIT expense component
-  </div>
-);
-
-const HelpPage = () =>(
-  <div>
-      This is from my HELP component
-  </div>
-);
-
-const NotFoundPage = () =>(
-  <div>
-      404 Not Found! - <Link to = "/">Go Home</Link>
-  </div>
-);
-
-const Header = () =>(
-  <header>
-      <h1>Expensify</h1>
-      <NavLink to = "/" activeClassName="is-active" exact ={true}>Dashboard</NavLink>
-      <NavLink to = "/create" activeClassName="is-active">Create Expense</NavLink>
-      <NavLink to = "/edit" activeClassName="is-active">Edit Expense</NavLink>
-      <NavLink to = "/help" activeClassName="is-active">Help</NavLink>
-  </header>
-);
 const AppRouter = () => (
   <BrowserRouter>
   <div>
