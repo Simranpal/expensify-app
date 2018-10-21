0f1b2f1e33bbb288d88e7cea1a229ab94229428a
Simranpal Singh <navigill20@gmail.com>
2018-10-21 16:44:46 -0700

    install redux and use createStore

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 //console.log(path.join(__dirname, 'public'));
 module.exports = {
-  entry : './src/app.js',
+  entry : './src/experiments/redux-101.js',
   output : {
     path : path.join(__dirname, 'public'),
     filename: 'bundle.js'
