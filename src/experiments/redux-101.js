76b08348d684c8f3c917c120b79ce51389688c38
Simran PC <navigill20@gmail.com>
2018-10-21 21:44:03 -0700

    use switch statement for choosing action

--- src/experiments/redux-101.js
+++ src/experiments/redux-101.js
@@ -2,13 +2,14 @@ import {createStore} from 'redux';
 
 const store = createStore((state = { count : 0 }, action)=>{
     
-    if(action.type === 'INCREMENT') {
-        return {
-            count : state.count + 1
-        }
-    }
-    else {
-        return state; 
+    switch(action.type) {
+        case 'INCREMENT' : 
+            return {
+                count : state.count + 1
+            };
+
+        default:
+            return state;
     }
 });
 
