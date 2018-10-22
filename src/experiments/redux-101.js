406e621ef8c39881397f3a233a62e1466fbd1704
Simran PC <navigill20@gmail.com>
2018-10-21 21:42:19 -0700

    update counter via dispatch call of sttore

--- src/experiments/redux-101.js
+++ src/experiments/redux-101.js
@@ -1,10 +1,28 @@
 import {createStore} from 'redux';
 
-const store = createStore((state = { count : 0 })=>{
-    return state;
+const store = createStore((state = { count : 0 }, action)=>{
+    
+    if(action.type === 'INCREMENT') {
+        return {
+            count : state.count + 1
+        }
+    }
+    else {
+        return state; 
+    }
 });
 
 console.log(store.getState());
 
 //Actions
 //increment count
+store.dispatch({
+    type : 'INCREMENT'
+});
+
+//another increment
+store.dispatch({
+    type : 'INCREMENT'
+});
+
+console.log(store.getState());
